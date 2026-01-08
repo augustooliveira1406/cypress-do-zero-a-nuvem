77627a930b6b609e2927830b36eef56d50520702
Augusto Oliveira <augustooliveira@MacBook-Pro-de-Augusto.local>
2026-01-08 15:30:44 -0300

    Configure cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -4,5 +4,7 @@ module.exports = defineConfig({
   viewportHeight: 880,
   viewportWidth: 1280,
   e2e: {},
+  //id do Cypress Cloud
+  projectId: "odd3p1",
 })
 
--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -185,7 +185,7 @@ describe('Central de Atendimento ao Cliente TAT', () => {
         .and('have.attr', 'target', '_blank')
   })
 
-  it('acessa a página da política de privacidade removendo o target e então clicando no link', () => {
+  it.only('acessa a página da política de privacidade removendo o target e então clicando no link', () => {
       cy.contains('a', 'Política de Privacidade')
         .invoke('removeAttr', 'target')
         .click()
@@ -193,8 +193,5 @@ describe('Central de Atendimento ao Cliente TAT', () => {
         cy.contains('h1', 'CAC TAT - Política de Privacidade').should('be.visible')
   })
 
-  
-
- 
 
 })
